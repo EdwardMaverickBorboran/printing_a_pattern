e829832fb2edae8f2e8420856c27d8635eb3b3df
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 20:21:32 +0800

    So far, the entire program does not have any error and it is working. The output displayed on the terminal however, it is only up until the 4th row even if the rows indicated were 5.

---
+++ printing_a_pattern.py
@@ -0,0 +1,23 @@
+# Create a program that will --
+
+# Print a pattern
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Expected results
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+
+# Number of rows need
+rows = 5
+
+# Using loop and range
+for numbers in range(rows):
+    for pattern in range(numbers):
+        print(numbers, end="")
+    print("")
